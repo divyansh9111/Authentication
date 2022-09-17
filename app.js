bf11dc181354f23de8abccb01985a50c9ed89367
divyansh <creatorsindori@gmail.com>
2022-09-17 22:32:11 +0530

    level 5 security (cookies and sessions)

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const mongoose=require("mongoose");
 const path = require("path");
 const ejs=require("ejs");
 // const encrypt=require("mongoose-encryption");
-// const md5=require("md5");//level 3 security
-const bcrypt=require("bcrypt");//level 4 security
-const saltRounds=10;
+// const md5=require("md5");//level 3 security 
+const passport=require("passport"); 
+const session=require("express-session"); 
+const passportLocalMongoose=require("passport-local-mongoose"); 
 const port=3000;
 const app=express();
 
@@ -16,11 +17,18 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-// secret string
-const secret=process.env.SECRET;
+// imp to use here
+app.use(session({
+    secret:"My Name Is Anthony",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true})
-
+// mongoose.set("useCreateIndex",true);
 // const userSchema=mongoose.Schema({
 //     email: {
 //         type: String,
@@ -35,19 +43,20 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true})
 const userSchema=new mongoose.Schema({
     email: {
         type: String,
-        required: true,
       },
     password: {
         type: String,
-        required: true,
       }
 });
 
+userSchema.plugin(passportLocalMongoose);
 
-// // Always add this step before creating a model and we are encrypting only password here, if email is also encrypted then we'll not be able to find the user in the database.
-// userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});//level 2 security
+const User=new mongoose.model("User",userSchema);
 
-const User=mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",(req,res)=>{
     res.render("Home");
@@ -58,44 +67,49 @@ app.get("/login",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.render("register");
 });
-
+app.get("/secrets",(req,res)=>{
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+app.get("/logout",(req,res)=>{
+    req.logout((err)=>{
+        console.log(err);
+    });
+    res.redirect("/");
+});
 
 app.post("/register",(req,res)=>{
-    bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{//level 4 security
-      const newUser=User({
-        email:req.body.username,
-        password:hash,
-    });
-    newUser.save((err)=>{
-        if (err) {
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
-    });   
-    });
-   
+   User.register({username:req.body.username},req.body.password,(err,user)=>{
+    if (err) {
+        console.log(err);
+        res.redirect("/register");
+    }else{
+        passport.authenticate("local")(req,res,()=>{
+            res.redirect("/secrets");
+        });
+    }
+
+   });
 });
 
 app.post("/login",(req,res)=>{
-    const username=req.body.username;
-    const password=req.body.password;
-
-    User.findOne({email:username},(err,foundUser)=>{
+    const user= new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+    req.login(user,(err)=>{
         if (err) {
             console.log(err);
+            res.redirect("/login");
         }else{
-            if (foundUser) {//level 5  security
-                bcrypt.compare(password,foundUser.password,(err,result)=>{
-                    if (result===true) {
-                        res.render("secrets");        
-                    }
-                });
-                
-            }
+            passport.authenticate("local")(req,res,()=>{
+                res.redirect("/secrets");
+            });
         }
     });
-
 });
 
 
